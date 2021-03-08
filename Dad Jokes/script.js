70f4c193bae0095ac6ea480091b805d3cc413b16
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2021-03-08 10:10:29 +0300

    Dad Jokes

--- Dad Jokes/script.js
+++ Dad Jokes/script.js
@@ -3,16 +3,30 @@ const jokeBtn = document.getElementById('jokeBtn')
 
 
 
-function generateJoke() {
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json'
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {jokeEl.innerHTML = data.joke})
+// }
+
+async function generateJoke() {
     const config = {
         headers: {
             Accept: 'application/json'
         },
     }
 
-    fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {jokeEl.innerHTML = data.joke})
+   const res = await fetch ('https://icanhazdadjoke.com', config)
+
+   const data = await res.json()
+
+   jokeEl.innerHTML = data.joke
 }
 
 jokeBtn.addEventListener('click', generateJoke)
