476577f66f46103b2f158c7c9d995990d314eee5
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2021-03-09 20:09:04 +0300

    Button Ripple Effect

---
+++ Button Ripple Effect/script.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('.ripple')
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        console.log(buttonTop, buttonLeft)
+
+    })
+})
