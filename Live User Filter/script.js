de78b8cad5d50df404f153fd3b6534279162d113
Fred <nyakagwa.fred@gmail.com>
2021-07-22 22:33:53 +0300

    Live user filter

---
+++ Live User Filter/script.js
@@ -0,0 +1,36 @@
+const result = document.getElementById('result');
+const filter = document.getElementById('filter');
+const listItems = [];
+
+async function getData() {
+	const res = await fetch('https://randomuser.me/api?results=20');
+	const { results } = await res.json();
+	result.innerHTML = '';
+	results.forEach((user) => {
+		const li = document.createElement('li');
+
+		listItems.push(li);
+		li.innerHTML = `
+        <img src="${user.picture.large}" alt="${user.name.first}">
+        <div class="user-info">
+            <h4>${user.name.first} ${user.name.last}</h4>
+            <p>${user.location.city}, ${user.location.country}</p>
+        </div>
+        `;
+		result.appendChild(li);
+	});
+}
+
+const filterData = (searchTerm) => {
+	listItems.forEach((item) => {
+		if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+			item.classList.remove('hide');
+		} else {
+			item.classList.add('hide');
+		}
+	});
+};
+
+filter.addEventListener('input', (e) => filterData(e.target.value));
+
+getData();
